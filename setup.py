23fd01e69cf579154dd542ac1d1617e4c25c6cc1
yiwen song <yiwen.song@airtable.com>
2023-09-21 10:33:50 -0700

    update requirements etc

--- setup.py
+++ setup.py
@@ -1,35 +1,36 @@
+"""setup.py"""
 import setuptools
 
-with open('README.md', 'r') as readme:
+with open("README.md", "r", encoding="utf8") as readme:
     README_TEXT = readme.read()
 
 setuptools.setup(
-    name='md-resume',
-    version='0.2.7',
-    license='MIT',
-    author='yiwen song',
-    author_email='songzgy@gmail.com',
-    url='https://github.com/yiwensong/resume',
-    description='markdown resume prettifier',
+    name="md-resume",
+    version="0.2.7",
+    license="MIT",
+    author="yiwen song",
+    author_email="songzgy@gmail.com",
+    url="https://github.com/yiwensong/resume",
+    description="markdown resume prettifier",
     long_description=README_TEXT,
-    long_description_content_type='text/markdown',
-    packages=setuptools.find_packages(exclude=['tests*']),
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(exclude=["tests*"]),
     install_requires=[
-        'Markdown',
+        "Markdown",
     ],
     classifiers=[
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.7',
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords=[
-        'resume',
-        'markdown',
-        'pretty',
-        'yiwen',
+        "resume",
+        "markdown",
+        "pretty",
+        "yiwen",
     ],
     entry_points={
-        'console_scripts': [
-            'generate-resume-pdf = md_resume:main',
+        "console_scripts": [
+            "generate-resume-pdf = md_resume:main",
         ],
     },
 )
